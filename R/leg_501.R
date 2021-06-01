d02706af0200ac79aae9fde0a60004d67115fc1f
Matteo Miotto <matteo@iMac-di-Matteo.lan>
2021-06-01 18:29:48 +0200

    leg e set ok (forse)

--- R/leg_501.R
+++ R/leg_501.R
@@ -1,32 +1,62 @@
-leg_501 <- function(player1, player2) {
+#' @name leg_501
+#' @title Single leg 501 in a match
+#' @description This function is used as calculator for a single 501 leg
+#' @usage leg_501(player1, player2)
+#' @param player1 player1 name
+#' @param player2 player2 name
+#' @returns A list containing:
+#' \item{winner}{character vector of the winner}
+#' \item{player1.stat}{Leg stats for player1}
+#' \item{player2.stat}{Leg stats for player2}
+#' @author Matteo Miotto
+#' @importFrom svDialogs dlg_form
+#' @importFrom stringr str_split
+#' @importFrom dplyr all_of
+#' @export
+leg_501 <- function(player1, player2, n.leg.set, set.id, match.id, n.leg.match) {
+
+  # leg id
+    sytime <- paste(unlist(str_split(
+      paste(unlist(str_split(
+      paste(unlist(str_split(Sys.time(), pattern = c(" "))), collapse = ""),
+      pattern = c("-"))), collapse = ""),
+      pattern = c(":"))), collapse = "")
+    l.ID <- paste("l", sytime, sep = "")
 
   # set useful values
   p1.score <- p2.score <- 501
-  p1.thrown_darts_score <- p1.first_dart <- p1.second_dart <- p1.third_dart <- p1.missed_doubles <- p1.checkout <- p1.closing_double <-
-    p2.thrown_darts_score <- p2.first_dart <- p2.second_dart <- p2.third_dart <- p2.missed_doubles <- p2.checkout <- p2.closing_double <- NULL
+  p1.missed_doubles <- p1.closing_double <- p2.missed_doubles <- p2.closing_double <- NA
+  p1.thrown_darts_score <- p1.checkout <- p2.thrown_darts_score <- p2.checkout <- NULL
   from_chr_to_score_vector <- c(0, 1:20, 1:20, (1:20)*2, (1:20)*3, 25, 50)
   from_chr_to_score_names  <- c("0", paste("o",(1:20), sep = ""), paste("i",(1:20), sep = ""), paste("d", c(1:20), sep = ""), paste("t", c(1:20), sep = ""), "25", "d25")
   names(from_chr_to_score_vector) <- from_chr_to_score_names
   p1.n_of_darts <- p1.busted <- p1.missed <- p1.100 <- p1.140 <- p1.180 <-
     p2.n_of_darts <- p2.busted <- p2.missed <- p2.100 <- p2.140 <- p2.180 <- 0
   doubles <- c((1:20)*2, 50)
+  levels <- c("0", as.character(1:20) ,paste("d", c(1:20), sep = ""), paste("t", c(1:20), sep = ""), "25", "d25")
 
   while ((p1.score !=0) & (p2.score !=0)) {
 
-    # player 1
-    # ask for scored points for 3 darts
-    form <- list("Darts score:TXT" = "")
-    form_title <- paste(player1, "score is:", as.character(p1.score))
-    p1.hand_score <- unlist(dlg_form(form, message = "Insert the value for the thrown darts separated by a comma (,) in the following form:
-                                      o#: for outer singles
-                                      i#: for inner singles
-                                      d#: for doubles
-                                      t#: for triples",
-                                  title = form_title)$res)
+    p1.hand_scores_chr <- p2.hand_scores_chr <- NA
 
-    # extrapolate dart score as chr and num vector
-    p1.hand_scores_chr <- as.character( trimws( unlist( strsplit(p1.hand_score, split = ",")), which = "both"))
-    p1.hand_scores_num <- as.numeric(from_chr_to_score_vector[c(p1.hand_scores_chr)])
+    # player 1
+      while (!all(all_of(p1.hand_scores_chr %in% from_chr_to_score_names))){
+        # ask for scored points for 3 darts
+        form <- list("Darts score:TXT" = "")
+        form_title <- paste(player1, "score is:", as.character(p1.score))
+        p1.hand_score <- unlist(dlg_form(form, message = "Insert the value for the thrown darts separated by a comma (,) in the following form:
+                                          o#: for outer singles
+                                          i#: for inner singles
+                                          d#: for doubles
+                                          t#: for triples",
+                                      title = form_title)$res)
+
+        # extrapolate dart score as chr and num vector
+        p1.hand_scores_chr <- as.character( trimws( unlist( strsplit(p1.hand_score, split = ",")), which = "both"))
+        p1.hand_scores_num <- as.numeric(from_chr_to_score_vector[c(p1.hand_scores_chr)])
+
+        if (!all(all_of(p1.hand_scores_chr %in% from_chr_to_score_names))) {message("Error: you have inserted unvalid values. Retry")}
+      }
 
     # subtract darts score from score and evaluating if miss double for checkout or bust
     p1.score_i <- p1.score
@@ -56,17 +86,6 @@ leg_501 <- function(player1, player2) {
 
     }
 
-    # add chr darts to each dart vector
-    for (i in seq_along(p1.hand_scores_chr)){
-
-      sentence <- switch(i,
-                         "1" = "p1.first_dart <- c(p1.first_dart, p1.hand_scores_chr[1])",
-                         "2" = "p1.second_dart <- c(p1.second_dart, p1.hand_scores_chr[2])",
-                         "3" = "p1.third_dart <- c(p1.third_dart, p1.hand_scores_chr[3])"
-      )
-      eval(parse(text = sentence))
-    }
-
     # add chr darts to thrown dart score
     p1.thrown_darts_score <- c(p1.thrown_darts_score, p1.hand_scores_chr)
 
@@ -78,25 +97,30 @@ leg_501 <- function(player1, player2) {
     if (p1.score == 0){
       p1.checkout <- sum(p1.hand_scores_num)
       p1.closing_double <- p1.hand_scores_chr[length(p1.hand_scores_chr)]
+      p2.checkout <- NA
       next
     }
 
 
 
     # player 2
-    # ask for scored points for 3 darts
-    form <- list("Darts score:TXT" = "")
-    form_title <- paste(player2, "score is:", as.character(p2.score))
-    p2.hand_score <- unlist(dlg_form(form, message = "Insert the value for the thrown darts separated by a comma (,) in the following form:
-                                      o#: for outer singles
-                                      i#: for inner singles
-                                      d#: for doubles
-                                      t#: for triples",
-                                     title = form_title)$res)
+    while (!all(all_of(p2.hand_scores_chr %in% from_chr_to_score_names))){
+      # ask for scored points for 3 darts
+      form <- list("Darts score:TXT" = "")
+      form_title <- paste(player2, "score is:", as.character(p2.score))
+      p2.hand_score <- unlist(dlg_form(form, message = "Insert the value for the thrown darts separated by a comma (,) in the following form:
+                                        o#: for outer singles
+                                        i#: for inner singles
+                                        d#: for doubles
+                                        t#: for triples",
+                                       title = form_title)$res)
 
-    # extrapolate dart score as chr and num vector
-    p2.hand_scores_chr <- as.character( trimws( unlist( strsplit(p2.hand_score, split = ",")), which = "both"))
-    p2.hand_scores_num <- as.numeric(from_chr_to_score_vector[c(p2.hand_scores_chr)])
+      # extrapolate dart score as chr and num vector
+      p2.hand_scores_chr <- as.character( trimws( unlist( strsplit(p2.hand_score, split = ",")), which = "both"))
+      p2.hand_scores_num <- as.numeric(from_chr_to_score_vector[c(p2.hand_scores_chr)])
+
+      if (!all(all_of(p2.hand_scores_chr %in% from_chr_to_score_names))) {message("Error: you have inserted unvalid values. Retry")}
+    }
 
     # subtract darts score from score and evaluating if miss double for checkout or bust
     p2.score_i <- p2.score
@@ -126,17 +150,6 @@ leg_501 <- function(player1, player2) {
 
     }
 
-    # add chr darts to each dart vector
-    for (i in seq_along(p2.hand_scores_chr)){
-
-      sentence <- switch(i,
-                         "1" = "p2.first_dart <- c(p2.first_dart, p2.hand_scores_chr[1])",
-                         "2" = "p2.second_dart <- c(p2.second_dart, p2.hand_scores_chr[2])",
-                         "3" = "p2.third_dart <- c(p2.third_dart, p2.hand_scores_chr[3])"
-      )
-      eval(parse(text = sentence))
-    }
-
     # add chr darts to thrown dart score
     p2.thrown_darts_score <- c(p2.thrown_darts_score, p2.hand_scores_chr)
 
@@ -148,47 +161,120 @@ leg_501 <- function(player1, player2) {
     if (p2.score == 0){
       p2.checkout <- sum(p2.hand_scores_num)
       p2.closing_double <- p2.hand_scores_chr[length(p2.hand_scores_chr)]
+      p1.checkout <- NA
     }
   }
 
-  p1.stats <- list("darts" = p1.thrown_darts_score,
-                   "1st dart" = p1.first_dart,
-                   "2nd dart" = p1.second_dart,
-                   "3rd dart" = p1.third_dart,
-                   "number of darts" = p1.n_of_darts,
-                   "180" = p1.180,
-                   "140+" = p1.140,
-                   "100+" = p1.100,
-                   "checkout" = p1.checkout,
-                   "closing double" = p1.closing_double,
-                   "missed doubles" = p1.missed_doubles,
-                   "missed" = p1.missed,
-                   "busted" = p1.busted
-  )
+  # Lists to return
+    # Df
+      p1.hand_dart_num <- rep(c(1, 2, 3), ceiling(length(p1.thrown_darts_score)/3))[1:length(p1.thrown_darts_score)]
+      p1.leg_dart_num  <- 1:length(p1.thrown_darts_score)
 
- p2.stats <- list("darts" = p2.thrown_darts_score,
-                  "1st dart" = p2.first_dart,
-                  "2nd dart" = p2.second_dart,
-                  "3rd dart" = p2.third_dart,
-                  "number of darts" = p2.n_of_darts,
-                  "180" = p2.180,
-                  "140+" = p2.140,
-                  "100+" = p2.100,
-                  "checkout" = p2.checkout,
-                  "closing double" = p2.closing_double,
-                  "missed doubles" = p2.missed_doubles,
-                  "missed" = p2.missed,
-                  "busted" = p2.busted
-  )
+      p1.darts.df <- data.frame("dart n in leg" = p1.leg_dart_num,
+                             "dart n in hand" = p1.hand_dart_num,
+                             "score" = p1.thrown_darts_score,
+                             "leg ID" = l.ID)
+      p1.power.df <- data.frame("leg ID" = l.ID,
+                                "100+" = p1.100,
+                                "140+" = p1.140,
+                                "180" = p1.180)
+      p1.checkout.df <- data.frame("leg ID" = l.ID,
+                                   "checkout" = p1.checkout,
+                                   "busted" = p1.busted)
 
- if (p1.score == 0) {winner <- player1} else {winner <- player2}
+        # doubles df
+          if (!is.na(p1.missed_doubles) & !is.na(p1.closing_double)) {
+            missed.df <- data.frame(scores = p1.missed_doubles) %>%
+              group_by(scores) %>%
+              summarise(missed = n())
+            closing.df <- data.frame(scores = p1.closing_double) %>%
+              group_by(scores) %>%
+              summarise(hit = n())
+
+            doubles.df <- full_join(missed.df, closing.df)
+            doubles.df$hit[is.na(doubles.df$hit)] <- 0
+            doubles.df$missed[is.na(doubles.df$missed)] <- 0
+
+            p1.doubles.df <- doubles.df %>%
+              mutate(scores = factor(scores, levels = levels)) %>%
+              arrange(scores) %>%
+              mutate("leg ID" = l.ID)
+          } else {
+            p1.doubles.df <- data.frame("scores" = NA, "hit" = NA, "missed" = NA, "leg ID" = l.ID)
+          }
+
+
+      p2.hand_dart_num <- rep(c(1, 2, 3), ceiling(length(p2.thrown_darts_score)/3))[1:length(p2.thrown_darts_score)]
+      p2.leg_dart_num  <- 1:length(p2.thrown_darts_score)
+
+      p2.darts.df <- data.frame("dart n in leg" = p2.leg_dart_num,
+                                "dart n in hand" = p2.hand_dart_num,
+                                "score" = p2.thrown_darts_score,
+                                "leg ID" = l.ID)
+      p2.power.df <- data.frame("leg ID" = l.ID,
+                                `100+` = p2.100,
+                                "140+" = p2.140,
+                                "180" = p2.180)
+      p2.checkout.df <- data.frame("leg ID" = l.ID,
+                                   "checkout" = p2.checkout,
+                                   "busted" = p2.busted)
+
+      # doubles df
+      if (!is.na(p2.missed_doubles) & !is.na(p2.closing_double)) {
+        missed.df <- data.frame(scores = p2.missed_doubles) %>%
+          group_by(scores) %>%
+          summarise(missed = n())
+        closing.df <- data.frame(scores = p2.closing_double) %>%
+          group_by(scores) %>%
+          summarise(hit = n())
+
+        doubles.df <- full_join(missed.df, closing.df)
+        doubles.df$hit[is.na(doubles.df$hit)] <- 0
+        doubles.df$missed[is.na(doubles.df$missed)] <- 0
+
+        p2.doubles.df <- doubles.df %>%
+          mutate(scores = factor(scores, levels = levels)) %>%
+          arrange(scores) %>%
+          mutate("leg ID" = l.ID)
+      } else {
+        p2.doubles.df <- data.frame("scores" = NA, "hit" = NA, "missed" = NA, "leg ID" = l.ID)
+      }
+
+
+      p1.stats <- list(
+        "darts" = p1.darts.df,
+        "power scoring" = p1.power.df,
+        "checkout" = p1.checkout.df,
+        "doubles" = p1.doubles.df
+      )
+
+      p2.stats <- list(
+        "darts" = p2.darts.df,
+        "power scoring" = p2.power.df,
+        "checkout" = p2.checkout.df,
+        "doubles" = p2.doubles.df
+      )
+      if (p1.score == 0) {winner <- player1} else {winner <- player2}
+
+      # leg df
+      leg.stat.df <- data.frame("leg ID" = l.ID,
+                                "player 1" = player1,
+                                "player 2" = player2,
+                                "winner" = winner,
+                                "n leg in set" = n.leg.set,
+                                "set ID" = set.id,
+                                "n leg in match" = n.leg.match,
+                                "match ID" = match.id)
+
+
 
  # return
- t <- paste("legres <- list('winner' = winner,", player1, ".leg.stats", "= p1.stats,", player2, ".leg.stats", "= p2.stats)", sep ="")
+
+ t <- paste("legres <- list('leg.stat' = leg.stat.df,", player1, ".leg.stats", "= p1.stats,", player2, ".leg.stats", "= p2.stats)", sep ="")
 
  eval(parse(text = t))
 
- cat(paste("Congratulations", winner), "You've won this leg", "\n")
+ cat(paste("Congratulations ", winner, "!", sep =""), "You've won this leg", "\n")
 
  return(legres)
 }
--- R/set_501.R
+++ R/set_501.R
@@ -1,31 +1,55 @@
-set_501 <- function(player1, player2, nlegs = 1, which_set){
+#' @name set_501
+#' @title Single 501 set in a match
+#' @description This function is used as calculator for a single 501 set
+#' @usage set_501(player1, player2, nlegs, which_set, match.id, which_leg_match)
+#' @param player1 player1 name
+#' @param player2 player2 name
+#' @returns A list containing:
+#' \item{set.stat}{set statistics}
+#' \item{player1.set.stat}{Set stats for player1}
+#' \item{player2.set.stat}{Set stats for player2}
+#' \item{set.legs.stat}{Legs statistics}
+#' @author Matteo Miotto
+#' @importFrom svDialogs dlg_form
+#' @importFrom stringr str_split
+#' @importFrom dplyr all_of
+#' @export
+
+set_501 <- function(player1, player2, nlegs = 1, which_set, match.id, which_leg_match){
 
   # set useful values
   p1.legs <- p2.legs <- 0
-  which_leg <- 1
+  which_leg_set <- 1
+  sytime <- paste(unlist(str_split(
+    paste(unlist(str_split(
+      paste(unlist(str_split(Sys.time(), pattern = c(" "))), collapse = ""),
+      pattern = c("-"))), collapse = ""),
+    pattern = c(":"))), collapse = "")
+  s.ID <- paste("s", sytime, sep = "")
 
   while ((p1.legs != nlegs) & (p2.legs != nlegs)) {
 
     # leg
-    cat(paste("Leg", which_leg), "Game on!", "\n")
+    cat(paste("Leg", which_leg_set), "Game on!", "\n")
     Sys.sleep(2)
 
     # change players turn
-    if (which_leg%%2 == 0) {
+    if (which_leg_set%%2 == 0) {
       play1 <- player2
       play2 <- player1} else {
         play1 <- player1
         play2 <- player2
       }
 
-    text1 <- paste("s", which_set, "l", which_leg, "<- leg_501(play1, play2)", sep = "")
+    text1 <- paste("s", which_set, "l", which_leg_set, "<- leg_501(play1, play2, n.leg.set = which_leg_set, set.id = s.ID, match.id = match.id, n.leg.match = which_leg_match)" ,sep = "")
     eval(parse(text = text1))
 
-    text2 <- paste("s", which_set, "l", which_leg, sep = "")
+    text2 <- paste("s", which_set, "l", which_leg_set, sep = "")
 
     # leg count
-    if (eval(parse(text = text2))$winner == player1) {p1.legs <- p1.legs + 1} else {p2.legs <- p2.legs + 1}
-    which_leg <- which_leg + 1
+    if (eval(parse(text = text2))$leg.stat$winner == player1) {p1.legs <- p1.legs + 1} else {p2.legs <- p2.legs + 1}
+    which_leg_set <- which_leg_set + 1
+    which_leg_match <- which_leg_match + 1
 
   }
 
@@ -38,26 +62,45 @@ set_501 <- function(player1, player2, nlegs = 1, which_set){
       set_legs <- str_subset(ls(), paste("s", which_set, sep = ""))
 
       # retrieve first leg stat
+      t.leg1.stat <- paste("s", which_set, "legs.stat <- s",  which_set, "l1$leg.stat", sep = "")
       text.stat <- paste("s", which_set, "l1", sep = "")
-      t1 <- paste(player1, ".set.stats <- eval(parse(text = text.stat))$", player1, ".leg.stats" , sep = "")
-      t2 <- paste(player2, ".set.stats <- eval(parse(text = text.stat))$", player2, ".leg.stats" , sep = "")
-      eval(parse(text = t1))
-      eval(parse(text = t2))
+      t.p1.leg1.stat <- paste(player1, ".set.stats <- eval(parse(text = text.stat))$", player1, ".leg.stats" , sep = "")
+      t.p2.leg1.stat <- paste(player2, ".set.stats <- eval(parse(text = text.stat))$", player2, ".leg.stats" , sep = "")
+
+      eval(parse(text = t.leg1.stat))
+      eval(parse(text = t.p1.leg1.stat))
+      eval(parse(text = t.p2.leg1.stat))
 
       # merge with other legs
-      for (i in 2:length(set_legs)) {
-        text.stat2 <- paste("s", which_set, "l", i, sep = "")
-        t3 <- paste(player1, ".set.stats <- mapply(c,", player1, ".set.stats, eval(parse(text = text.stat2))$", player1, ".leg.stats)", sep = "")
-        t4 <- paste(player2, ".set.stats <- mapply(c,", player2, ".set.stats, eval(parse(text = text.stat2))$", player2, ".leg.stats)", sep = "")
-        eval(parse(text = t3))
-        eval(parse(text = t4))
+      for (i in 2:length(set_legs))
+        t.leg <- paste("s", which_set, "legs.stat <- rbind(s", which_set, "legs.stat, s", which_set, "l", i, "$leg.stat)", sep = "")
+      eval(parse(text = t.leg))
+
+      for (j in 1:length(eval(parse(text = paste("s", which_set, "l", i, "$", player1, ".leg.stats", sep = ""))))) {
+        t.p1.l <- paste(player1, ".set.stats[[", j, "]] <- rbind(", player1, ".set.stats[[", j, "]],", "s", which_set, "l", i, "$", player1, ".leg.stats[[", j, "]])", sep ="")
+        t.p2.l <- paste(player2, ".set.stats[[", j, "]] <- rbind(", player2, ".set.stats[[", j, "]],", "s", which_set, "l", i, "$", player2, ".leg.stats[[", j, "]])", sep ="")
+        eval(parse(text = t.p1.l))
+        eval(parse(text = t.p2.l))
+
       }
 
-  cat(paste("Congratulations", winner), "You've won this set")
+      # set df
+      set.stat.df <- data.frame(
+        "set.ID" = s.ID,
+        "player.1" = player1,
+        "player.2" = player2,
+        "winner" = winner,
+        "n.set.match" = which_set,
+        "match.ID" = match.id,
+        "n.leg.to.win" = nlegs,
+        "n.played.legs" = which_leg_set - 1
+      )
+
+  cat(paste("Congratulations ", winner, "!", sep = ""), "You've won this set")
 
   # return
-  tres <- paste("setres <- list('winner' = winner,", player1, ".set.stats =", player1, ".set.stats,",
-                player2, ".set.stats =", player2, ".set.stats)", sep = "")
+  tres <- paste("setres <- list('set.stat' = set.stat.df,", player1, ".set.stats =", player1, ".set.stats,",
+                player2, ".set.stats =", player2, ".set.stats, s", which_set, ".legs.stat = s", which_set, "legs.stat)", sep = "")
 
   eval(parse(text = tres))
 
