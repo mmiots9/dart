1dc3b41918951a7354a752686947bd863294c128
Matteo Miotto pro <matteo.miotto9@gmail.com>
2021-10-20 14:55:47 +0200

    Add man pages for getGeneric functions

--- R/getGeneric.R
+++ R/getGeneric.R
@@ -1,4 +1,10 @@
 # getPlayers
+  #' @name getPlayers
+  #' @title Get player/s name/s
+  #' @usage getPlayers(input)
+  #' @param input A leg1p, leg2p, set1p, set2p, match1p or match2p class object
+  #' @returns Character vector containing player/s name/s
+  #' @author Matteo Miotto
   #' @export
   setGeneric(
     "getPlayers",
@@ -10,6 +16,11 @@
 
 
 # getID
+  #' @name getID
+  #' @title Get leg/set/match ID
+  #' @usage getID(input)
+  #' @param input A leg1p, leg2p, set1p, set2p, match1p or match2p class object
+  #' @returns Character vector containing leg, set or match ID
   #' @export
   setGeneric(
     "getID",
@@ -21,6 +32,11 @@
 
 
 # getWin
+  #' @name getWin
+  #' @title Win or lost
+  #' @usage getWin(input)
+  #' @param input A leg1p, leg2p, set1p, set2p, match1p or match2p class object
+  #' @returns 0 if lost, 1 if won
   #' @export
   setGeneric(
     "getWin",
@@ -32,6 +48,11 @@
 
 
 # getStats
+  #' @name getStats
+  #' @title Get leg/set/match stats
+  #' @usage getStats(input)
+  #' @param input A leg1p, set1p or match1p class object
+  #' @returns A list containing leg, set or match stats (means, checkouts, doubles, power scoring, number of darts and number of legs/sets won and lost)
   #' @export
   setGeneric(
     "getStats",
@@ -43,6 +64,11 @@
 
 
 # getDate
+  #' @name getDate
+  #' @title Get leg/set/match date of play
+  #' @usage getDate(input)
+  #' @param input A leg1p, leg2p, set1p, set2p, match1p or match2p class object
+  #' @returns Character vector containing the date of play (YYYY-MM-DD)
   #' @export
   setGeneric(
     "getDate",
@@ -53,6 +79,11 @@
   )
 
 # getWinner
+  #' @name getWinner
+  #' @title Get leg/set/match winner
+  #' @usage getWinner(input)
+  #' @param input A leg2p, set2p or match2p class object
+  #' @returns Character vector containing the name of the winner
   #' @export
   setGeneric(
     "getWinner",
